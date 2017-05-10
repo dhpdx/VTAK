d9c6aeadd8426f89634b68e9370400fa68375edc
Cameron <camdunne@gmail.com>
2017-05-10 11:24:15 -0400

    typo process.env

--- server/routes.js
+++ server/routes.js
@@ -14,7 +14,7 @@ var client = new Twitter({
 	consumer_key: process.env.CONSUMER_KEY,
 	consumer_secret: process.env.CONSUMER_SECRET,
   access_token_key: process.env.ACCESS_TOKEN_KEY,
-  access_token_secret: env.ACCESS_TOKEN_SECRET
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
 
