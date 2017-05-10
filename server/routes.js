dd0ade84dd2d45a5532f5fe3be0f53d731220388
Cameron <camdunne@gmail.com>
2017-05-10 11:23:12 -0400

    env

--- server/routes.js
+++ server/routes.js
@@ -11,10 +11,10 @@ var request = require('request');
 var io = require('./server.js');
 
 var client = new Twitter({
-	consumer_key: env.consumer_key,
-	consumer_secret: env.consumer_secret,
-  access_token_key: env.access_token_key,
-  access_token_secret: env.access_token_secret
+	consumer_key: process.env.CONSUMER_KEY,
+	consumer_secret: process.env.CONSUMER_SECRET,
+  access_token_key: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: env.ACCESS_TOKEN_SECRET
 });
 
 
